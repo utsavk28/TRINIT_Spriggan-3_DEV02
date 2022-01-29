671de274721f9d563b486baf5cc111da2d0c6d2f
esha <shahesha01@gmail.com>
2022-01-29 18:41:07 +0530

    user backend added

--- server/models/User.js
+++ server/models/User.js
@@ -16,6 +16,28 @@ const UserSchema = new Schema({
         type: String,
         required: true,
     },
+    contact: {
+        type: Number,
+        validate: {
+            validator: function(v) {
+                return /d{10}/.test(v);
+            },
+            message: '{VALUE} is not a valid 10 digit number!'
+        }
+    },
+    project:[
+        {
+            degisnation:{
+                type:String,
+                required:true,
+            },
+            organization:{
+                type:String,
+                required:true,
+            },
+        }
+    ],
+    
     date: {
         type: Date,
         default: Date.now,
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
+//const User1=require('../../models/User');
 const { check } = require('express-validator');
 
 const { registerUser } = require('../../controllers/users.js');
+const User = require('../../models/User');
 
 const router = express.Router();
 
@@ -18,4 +20,51 @@ router.post(
     registerUser
 );
 
+router.get('/',async(req,res)=>{
+    try{
+    const user=await User.find();
+    res.json(post);
+    }catch(err){
+        res.json({message:err});
+    }
+});
+//Getting details of particular user
+router.get('/:userId',async(req,res)=>{
+    try{
+    const user=await User.findById(req.params.userId);
+    res.json(post);
+    }catch(err){
+        res.json({message});
+    }
+});
+
+//updating the email
+router.patch('/:userId',async(req,res)=>{
+    try{
+        const updateUser=await User.updateOne({_id:req.params.userId},
+            {$set:{email:req.body.email}})
+    }catch(err){
+        res.json({message:err});
+    }
+});
+
+//updating the phone number
+router.patch('/:userId',async(req,res)=>{
+    try{
+        const updateUser=await User.updateOne({_id:req.params.userId},
+            {$set:{contact:req.body.contact}})
+    }catch(err){
+        res.json({message:err});
+    }
+});
+
+//Updating the password
+router.patch('/:userId',async(req,res)=>{
+    try{
+        const updateUser=await User.updateOne({_id:req.params.userId},
+            {$set:{password:req.body.password}})
+    }catch(err){
+        res.json({message:err});
+    }
+});
 module.exports = router;
