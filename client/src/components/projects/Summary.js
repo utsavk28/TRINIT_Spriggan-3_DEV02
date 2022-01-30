d1ae9ab8db899ac5b1557849ffad12edf0bc73d5
utsavk28 <utsavkhatu416@gmail.com>
2022-01-30 12:57:17 +0530

    Minor fixes

--- client/src/components/projects/Summary.js
+++ client/src/components/projects/Summary.js
@@ -1,46 +1,49 @@
-import React from 'react';
-// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-// import { Pie } from 'react-chartjs-2';
-// import { Button, Card, Col, Form, Row } from 'react-bootstrap';
-// import {data} from "./SummaryData";
-import ReactDOM from "react-dom";
-import Pdf from "react-to-pdf";
+import React, { useEffect, useState } from 'react';
+import Pdf from 'react-to-pdf';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { getAllBugsforProject, getProject } from '../../redux/actions/project';
 const ref = React.createRef();
 
-
-// ChartJS.register(ArcElement, Tooltip, Legend);
-
-
 const Summary = () => {
-  return (
-    <>
-    <div className='container-fluid'>
-    <Pdf targetRef={ref} filename="bugDetails.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-      </Pdf>
-      <div ref={ref}>
-        <h1>Bug Details</h1>
-        <h2>Bug 1</h2>
-        <h4>Title: Bug Test 1</h4>
-        <h5>Description: Bug 1 Description</h5>
-        <br />
-        <h2>Bug 2</h2>
-        <h4>Title: Bug Test 2</h4>
-        <h5>Description: Bug 2 Description</h5>
-        <h2>Bug 3</h2>
-        <h4>Title: Bug Test 3</h4>
-        <h5>Description: Bug 3 Description</h5>
-      </div>
+    const { projects, bugs } = useSelector((state) => state.project);
+    const dispatch = useDispatch();
+    const location = useLocation().pathname.split('/')[2];
 
-    </div>
-  
-    {/* <div style={{height:"550px", width:"550px"}}>
+    useEffect(() => {
+        dispatch(getAllBugsforProject(location));
+        dispatch(getProject(location));
+    }, [location]);
+    return (
+        <>
+            <div className='container-fluid'>
+                <Pdf targetRef={ref} filename='bugDetails.pdf'>
+                    {({ toPdf }) => (
+                        <button onClick={toPdf}>Generate Pdf</button>
+                    )}
+                </Pdf>
+                <div ref={ref}>
+                    <h1>Bug Details</h1>
+
+                    {bugs.map((bug) => {
+                        return (
+                            <div>
+                                <h3>{bug.title}</h3>
+                                <p>{bug.descriptions}</p>
+                                <br />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+
+            {/* <div style={{height:"550px", width:"550px"}}>
               
                     <Pie data={data} />;
               
             </div> */}
-    </>
-  );
+        </>
+    );
 };
 
 export default Summary;
